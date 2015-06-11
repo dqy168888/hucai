b234b832e5a92a79f1ceba52065c6f488a1ae780
wangdachui <471552503@qq.com>
2015-06-11 13:28:34 +0800

    ttt

--- caches/error_log.php
+++
@@ -1,4 +0,0 @@
-<?php exit;?>04-13 12:59:18 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1442
-<?php exit;?>04-13 12:59:18 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1443
-<?php exit;?>04-13 13:16:05 | 2048 | Only variables should be passed by reference | phpcms\modules\member\index.php | 228
-<?php exit;?>04-13 17:19:42 | 8192 | Function split() is deprecated | caches\caches_model\caches_data\content_update.class.php | 70
